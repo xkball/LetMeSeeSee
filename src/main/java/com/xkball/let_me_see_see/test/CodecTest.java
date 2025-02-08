24481bff2331699a8e41b33827b0a0adb5e9e5c1
xkball <45330674+xkball@users.noreply.github.com>
2025-02-09 01:40:02 +0800

    试图支持DSLT
    Took 48 minutes

---
+++ src/main/java/com/xkball/let_me_see_see/test/CodecTest.java
@@ -0,0 +1,15 @@
+package com.xkball.let_me_see_see.test;
+
+import io.netty.buffer.ByteBufAllocator;
+import net.minecraft.network.codec.ByteBufCodecs;
+
+public class CodecTest {
+    
+    public static void main(String[] args) {
+        var buf = ByteBufAllocator.DEFAULT.buffer(8);
+        ByteBufCodecs.VAR_INT.encode(buf,-1);
+        for(var i = 0; i < buf.capacity(); i++) {
+            System.out.println(buf.getByte(i));
+        }
+    }
+}
