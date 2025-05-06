339821f9f3c6c489c0708fb1e84296bbb4cad821
xkball <45330674+xkball@users.noreply.github.com>
2025-05-06 14:07:31 +0800

    更新物品栏图标和i18n文件
    Took 54 seconds
    
    (cherry picked from commit 7a9898f182acb63c9911f3f0467a74c226025d5b)
    
    Took 14 minutes

--- src/main/java/com/xkball/let_me_see_see/common/item/LMSItems.java
+++ src/main/java/com/xkball/let_me_see_see/common/item/LMSItems.java
@@ -6,7 +6,6 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -21,16 +20,16 @@ public class LMSItems {
     
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, LetMeSeeSee.MODID);
     
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> THE_TAB = CREATIVE_MODE_TABS.register("tab", () -> CreativeModeTab.builder()
-            .title(Component.translatable("itemGroup.let_me_see_see"))
-            .icon(Items.APPLE::getDefaultInstance)
-            .withTabsBefore(CreativeModeTabs.FOOD_AND_DRINKS, CreativeModeTabs.INGREDIENTS, CreativeModeTabs.SPAWN_EGGS)
-            .build());
-    
     public static final DeferredHolder<Item, ScannerItem> SCANNER = ITEMS.register("scanner", () -> new ScannerItem(new Item.Properties().stacksTo(1)));
     public static final DeferredHolder<Item, RetrieverItem> RETRIEVER = ITEMS.register("retriever", () -> new RetrieverItem(new Item.Properties().stacksTo(1)));
     public static final DeferredHolder<Item, DataBaseItem> DATA_BASE = ITEMS.register("data_base", () -> new DataBaseItem(new Item.Properties().stacksTo(1)));
     public static final DeferredHolder<Item, ItemDataExporterItem> ITEM_ITEM_DATA_EXPORTER = ITEMS.register("item_data_exporter", () -> new ItemDataExporterItem(new Item.Properties().stacksTo(1)));
+    
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> THE_TAB = CREATIVE_MODE_TABS.register("tab", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup.let_me_see_see"))
+            .icon(() -> SCANNER.get().getDefaultInstance())
+            .withTabsBefore(CreativeModeTabs.FOOD_AND_DRINKS, CreativeModeTabs.INGREDIENTS, CreativeModeTabs.SPAWN_EGGS)
+            .build());
     
     public static void init(IEventBus eventBus) {
         ITEMS.register(eventBus);
