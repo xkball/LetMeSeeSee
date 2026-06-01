cad39316e8797f54081b051a109f453e4c5e6063
xkball <45330674+xkball@users.noreply.github.com>
2026-06-01 15:01:51 +0800

    porting
    Took 13 seconds

---
+++ src/main/java/com/xkball/let_me_see_see/client/ScreenProviders.java
@@ -0,0 +1,26 @@
+package com.xkball.let_me_see_see.client;
+
+import com.xkball.let_me_see_see.client.gui.screen.ExplorerScreen;
+import com.xkball.let_me_see_see.client.gui.screen.ItemDataExporterScreen;
+import com.xkball.let_me_see_see.common.item.LMSItems;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ScreenProviders {
+
+    public static final Map<Identifier, IScreenProviderItemClient> PROVIDERS = new HashMap<>();
+
+    public static void init() {
+        PROVIDERS.put(LMSItems.EXPLORER.getId(), (stack, slot) -> new ExplorerScreen());
+        PROVIDERS.put(LMSItems.ITEM_ITEM_DATA_EXPORTER.getId(), (stack, slot) -> new ItemDataExporterScreen());
+    }
+
+    public interface IScreenProviderItemClient {
+        Screen createScreen(ItemStack stack, EquipmentSlot slot);
+    }
+}
